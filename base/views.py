1c5d921a39a3c404aa1bf68e434fcdafb1db6e71
project <world.wide.eunseo@gmail.com>
2024-03-10 10:43:06 +0900

    Feat: 작업 삭제 페이지 추가

--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 from django.urls import reverse_lazy
 
 from .models import Task
@@ -28,4 +29,10 @@ class TaskCreate(CreateView):
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
