593029e0ac30941ab035c121b2636e435a87d3d3
project <world.wide.eunseo@gmail.com>
2024-03-11 00:37:54 +0900

    Chore: nginx 배포 준비

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -140,16 +140,16 @@ LOGIN_URL = 'login'
 
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+#
 # STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'base/static/base/css'),
+#     os.path.join(BASE_DIR, 'base/templates/base/static'),
 # ]
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'# The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'# The URL to use when referring to static files (where they will be served from)
+STATIC_ROOT = '/var/www/todolist/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
